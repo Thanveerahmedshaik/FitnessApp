41465e5a605a54925f137fe62c3c78d7e8f44736
Thanveer ahmed Shaik <shaikthanveerahmed123@gmail.com>
2025-02-10 04:12:35 +0200

    second commit

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Swift UI app baby")
+            Text("Second commit")
         }
         .padding()
     }
