93eeab01a962ef0bb251dce368b6fededd2d4981
Thanveer ahmed Shaik <shaikthanveerahmed123@gmail.com>
2025-02-10 04:17:34 +0200

    APP1: HomeScreen text Fix

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Second commit")
+            Text("HomeScreen text fix")
         }
         .padding()
     }
