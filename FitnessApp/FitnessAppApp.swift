98d1399a9c1a33856f410f9de0a8978c5fe090b6
Thanveer ahmed Shaik <shaikthanveerahmed123@gmail.com>
2025-02-10 02:54:29 +0200

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Thanveer ahmed Shaik on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
